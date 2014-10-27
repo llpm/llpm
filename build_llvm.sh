7779d8788207607a58a2c1f3d207543bcd139824
John Demme <jdd@cs.columbia.edu>
2014-10-27 15:30:23 -0400

    Pulling in clang

--- build_llvm.sh
+++ build_llvm.sh
@@ -10,9 +10,9 @@ cd ../../
 mkdir -p llvm_build
 cd llvm_build
 ../ext/llvm/configure --prefix=$insdir \
-    --enable-debug-runtime --enable-debug-symbols --enable-keep-symbols \
-    --enable-cxx11 --enable-libcpp \
-    --enable-jit --enable-doxygen
+    --with-clang-srcdir=../ext/clang \
+    --enable-cxx11 --enable-libcpp --enable-jit \
+    --enable-debug-runtime --enable-debug-symbols --enable-keep-symbols
 make -j16
 make install
 cd ..
