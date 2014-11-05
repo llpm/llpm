da03e265eb6a5d4f867ed74c117187cde920b741
John Demme <jdd@cs.columbia.edu>
2014-11-04 21:25:17 -0500

    Branch (which becomes select) test

---
+++ tests/verilator/branch1/simple.c
@@ -0,0 +1,6 @@
+long simple(long a, long b) {
+    if (a > b)
+        return a;
+    return b;
+}
+
---
+++ tests/verilator/branch1/simple_test.cpp
@@ -0,0 +1,16 @@
+#include "simple.hpp"
+
+#include <stdio.h>
+
+int main(void) {
+    simple* s = new simple();
+    s->trace("debug.vcd");
+    s->reset();
+    s->x(4, 10);
+    uint64_t l;
+    s->a(&l);
+    printf("Result: %lu\n", l);
+    s->run(5);
+    delete s;
+    return 0;
+}
