fea87a6f6f52ea090122d1b4cac6e45343f7317f
John Demme <jdd@cs.columbia.edu>
2014-12-18 23:59:47 -0500

    RegCounter test and ignores.

---
+++ tests/cpphdl/reg_counter/reg_counter_test.cpp
@@ -0,0 +1,19 @@
+#include "RegCounter.hpp"
+
+#include <cstdio>
+
+int main() {
+    RegCounter* rc = new RegCounter();
+    rc->trace("debug.vcd");
+    rc->reset();
+    uint64_t start = rc->cycles();
+    rc->set_req(0, 10);
+    char c;
+    rc->set_resp(&c);
+    uint64_t val = 666;
+    rc->read_req(0);
+    rc->read_resp(&val);
+    printf("Result: %lu\n", val);
+    printf("Cycle count: %lu\n", rc->cycles() - start);
+    return 0;
+}
